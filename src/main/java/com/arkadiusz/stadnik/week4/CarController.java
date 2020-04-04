b2536f94c2bd9b0acb8a22feba8b8338e3fa75c6
areka <arekadnik@gmail.com>
2020-04-04 19:19:38 +0200

    Thymeleaf

---
+++ src/main/java/com/arkadiusz/stadnik/week4/CarController.java
@@ -0,0 +1,17 @@
+package com.arkadiusz.stadnik.week4;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CarController {
+
+    @GetMapping("/car")
+    public String getCar(Model model){
+        Car car = new Car("BMW", "i8");
+        model.addAttribute("name","Arek");
+        model.addAttribute("car",car);
+        return "car";
+    }
+}
